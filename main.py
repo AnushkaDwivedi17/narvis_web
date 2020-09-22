78716802328cb934c9b0d2712710fdd5e6ae7655
Nayan Arora <nayanarora55@gmail.com>
2020-09-22 15:40:21 +0530

    UI done added database

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
 from flask import Flask , render_template
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+
 app=Flask(__name__)
 
+#app.config['SQLALCHEMY_DATABSE_URI']='mysql://root:@localhost/narvis'
+#db=SQLAlchemy(app)
+
+'''class devices(db.Model):
+    Xiaomi=db.Column(db.String(50),primary_key=True)
+    Asus=db.Column(db.String(50),nullable=False)
+    Realme=db.Column(db.String(50),nullable=False)
+    Motorola=db.Column(db.String(50),nullable=False)'''
+
+host='localhost'
+user = 'root'
+password = ''
+db = 'narvis'
+
 @app.route("/")
+
 def index ():
     return render_template('index.html')
-@app.route("/downloads")
-def downloads() :
-    return render_template('downloads.html')
-app.run(port=100)
+
+
+@app.route("/devices/",methods=['GET'])
+
+def devices() :
+    con = pymysql.connect(host=host,user=user,password=password,db=db, use_unicode=True, charset='utf8')
+    cur = con.cursor()
+    cur.execute("SELECT * FROM device")
+    device = cur.fetchall()
+    return render_template('devices.html', device=device)
+app.run(debug=True)  
+
+
+
+
+
